3a87519478ba3f2d86f8d5572e20594e15bc0f9e
Zerosart <36707169+Zerosart@users.noreply.github.com>
2018-08-16 21:14:02 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Set up the winston logger
+let wins = require('winston');
+let log_level = "debug";
+if (process.env.LOG_LEVEL) log_level = process.env.LOG_LEVEL;
+wins.configure({
+    transports: [
+        new wins.transports.Console({
+            format: wins.format.simple()
+        })
+    ],
+    level: log_level
+});
+
+// Terminate on unhandle Promise rejections
+process.on('unhandledRejection', e => {
+  wins.error("UNHANDLED PROMISE EXCEPTION");
+  wins.error(e);
+  process.exit(1);
+});
+
+// Routes
+let index = require('./routes/index');
+
+// Initial render of the frontend HTML
+index.renderFrontend();
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middleware for enforcing https on heroku
+var enforceHttps = (req, res, next) => {
+    if (req.header('x-forwarded-proto') != 'https') {
+        res.redirect(301, `https://${req.get('host')}${req.originalUrl}`);
+    } else {
+        next();
+    }
+};
+
+if (process.env.NODE_ENV == 'production') {
+  // Uncomment the line below to enforce HTTPS in production on Heroku
+  // app.use(enforceHttps);
+} else {
+  app.locals.pretty = true;
+  app.use((req, res, next) => {
+    // In dev mode every page load will re-render the frontend
+    index.renderFrontend();
+    next();
+  });
+}
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    // Note that the origin of an extension iframe will be null
+    // so the Access-Control-Allow-Origin has to be wildcard.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use('/', index);
+app.use('/frontend', express.static(path.join(__dirname, 'frontend')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
